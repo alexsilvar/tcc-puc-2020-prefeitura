fa28eb8ee235579039380275c7c41207f0c6611b
alexsilvar <ramosdasilva.alexsander@gmail.com>
2021-03-29 00:20:20 -0300

    update datasource

--- ms-auth/src/datasources/userspostgres.datasource.ts
+++ ms-auth/src/datasources/userspostgres.datasource.ts
@@ -4,13 +4,7 @@ import { juggler } from '@loopback/repository';
 const config = {
   name: 'userspostgres',
   connector: 'postgresql',
-  // postgres://kong@tcc-puc-postgres:SuperSecretPWD123@tcc-puc-postgres.postgres.database.azure.com:5432/postgres
   url: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE,
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.DB_DATABASE
 };
 
 // Observe application's life cycle to disconnect the datasource when
